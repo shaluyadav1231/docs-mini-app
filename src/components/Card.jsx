5a65e9ad40e6c8521de0adb7a22b6208b671d4b5
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-09-25 16:19:57 +0530

    carda completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
-function Card() {
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+function Card({data}) {
   return (
     <>
-          <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'></div>
+          <div className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden'>
+          <FaRegFileAlt />
+          <p className='text-sm mt-5 font-semibold leading-tight'>{data.desc}</p>
+          <div className='footer absolute bottom-0 w-full  left-0'>
+            <div className='flex items-center justify-between py-3 px-8  mb-3 '>
+                <h5>{data.filesize}</h5>
+                <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center '>
+                    {data.close ? <IoClose />: <LuDownload size=".7em" color="#fff" />}
+                   
+                </span>
+                
+            </div>
+            {data.tag.isOpen && ( <div className={`tag w-full py-4 ${data.tag.tagColor==="blue" ? "bg-blue-600": "bg-green-600"} flex items-center justify-center`}>
+                <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+            </div>) }
+           
+
+          </div>
+        
+
+          
+          
+          
+          </div>
     </>
   )
 }
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 function Forground() {
+    const data = [
+        {
+           desc:"This is the background color of the card that will be displayed",
+           filesize:".9mb",
+           close:false,
+           tag:{isOpen:true, tagTitle:"Download Now", tagColor:"green",} 
+        },
+        {
+           desc:"This is the background color of the card that will be displayed",
+           filesize:".9mb",
+           close:false,
+           tag:{isOpen:true, tagTitle:"Download Now", tagColor:"blue",} 
+        },
+        {
+           desc:"This is the background color of the card that will be displayed",
+           filesize:".9mb",
+           close:false,
+           tag:{isOpen:false, tagTitle:"Download Now", tagColor:"green",} 
+        }
+      
+    ]
+    useState()
   return (
     <>
     
-    <div className=' fixed z-[3] top-0 left-0 w-full h-full'>
-    <Card />
+    <div className=' fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap p-5' >
+   
+   {data.map((item,index)=>(
+    <Card data={item}/>
+   ))}
+       
     </div>
 
     
