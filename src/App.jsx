806085c4906c70e8c6e401d85a213c5a743f73b6
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-09-25 09:20:58 +0530

    background done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bcakground from './components/Bcakground'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800' >
+      <Bcakground />
+    </div>
+  )
+}
+
+export default App
